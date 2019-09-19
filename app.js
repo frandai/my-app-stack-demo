04472cd24d8aa15654815f7286055ec3d0e2d7ef
Francisco Serrano Carmona <francisco.serrano.carmona@ibm.com>
2019-09-19 09:07:51 +0200

    index modified

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const app = require("express")();
 
 app.get("/", (req, res) => {
-  res.send(
-    "<br/><br/><center><H1>Hello from Appsody!</H1><br/><p>A modification.</p></center>"
-  );
+  res.send("<br/><br/><center><H1>Hello from Appsody!</H1><br/></center>");
 });
 
 module.exports.app = app;
